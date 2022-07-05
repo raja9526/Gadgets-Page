c8a9168543a2e47f688c19d992f1c994523f4aa5
raja reddy <rajareddy9526@gmail.com>
2022-07-05 10:56:50 +0000

    Gadgets Page

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const app = express;
+
+const app = express();
 
 app.get("/gadgets", (request, response) => {
-  request.send("gadgets.html", { root: __dirname });
+  response.sendFile("./gadgets.html", { root: __dirname });
 });
 
 module.exports = app;
